d247c528844ffde2f7db7d1e50932f6492d3e565
Alexander Gharibian <alexandergharibian@pal-nat186-88-237.itap.purdue.edu>
2017-03-22 11:31:50 -0400

    Added some schemas

--- Pathfynder/PathFynder/collections/collections.js
+++ Pathfynder/PathFynder/collections/collections.js
@@ -1,3 +1,4 @@
+//The Collections
 Course = new Mongo.Collection('course');
 CourseReview = new Mongo.Collection('courseReview');
 Internship = new Mongo.Collection('internship');
@@ -8,3 +9,23 @@ Dining = new Mongo.Collection('dining');
 DiningReview = new Mongo.Collection('diningReview');
 Club = new Mongo.Collection('club');
 ClubReview = new Mongo.Collection('clubReview');
+
+
+//Schemas
+
+Course.schema = new SimpleSchema({
+    name: {type: String},
+    abrev: {type: String},
+    number: {type: Number},
+    avgWorkloadRating: {type: Number},
+    avgDifficultyRating: {type: Number},
+    avgUtilityRating: {type Number}
+});
+
+CourseReview.schema = new SimpleSchema({
+    date: {type: Date},
+    review: {type: String},
+    difficutlyRating: {type: Number},
+    workloadRating: {type: Number},
+    utilityRating: {type: Number}
+});
