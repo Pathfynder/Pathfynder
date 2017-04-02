48b0605a0ad789b1c4212b52c10416f3f7b5e4bd
Ahad <ssagheer@purdue.edu>
2017-04-02 12:14:18 -0400

    added booleans to make fields public/private

--- Pathfynder/PathFynder/server/main.js
+++ Pathfynder/PathFynder/server/main.js
@@ -69,5 +69,8 @@ Accounts.onCreateUser(function(options, user) {
     user.profile.university = '';
     user.profile.gradDate = '';
     user.profile.major = '';
+    user.profile.majorBool = false;
+    user.profile.usernameBool = false;
+    user.profile.gradDateBool = false;
     return user;
 });
