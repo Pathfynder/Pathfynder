1c0082b51b18f442f837b047d476be88202f951d
allenquanobi <nguye225@purdue.edu>
2017-04-06 18:13:55 -0400

    Fixed Mailgun thing

--- Pathfynder/PathFynder/server/main.js
+++ Pathfynder/PathFynder/server/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
     // code to run on server at startup
-    process.env.MAIL_URL="smtp://postmaster@pathfynder.ltd:cookies@smtp.mailgun.org:587";
+    process.env.MAIL_URL="smtp://postmaster@mail.pathfynder.ltd:040290bafa1f8586df07331ae666ea42@smtp.mailgun.org:587";
     console.log(process.env);
     Accounts.urls.verifyEmail = function (token) {
         return Meteor.absoluteUrl('verify-email/' + token);
